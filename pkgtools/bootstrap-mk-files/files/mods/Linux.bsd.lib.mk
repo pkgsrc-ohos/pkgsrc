5b7e4f7ca4e8ef3289914575675343b898016554
hqzing <hqzing@outlook.com>
2026-04-14 23:40:35 +0800

    add llvm-ranlib support for pkgsrc

--- pkgtools/bootstrap-mk-files/files/mods/Linux.bsd.lib.mk
+++ pkgtools/bootstrap-mk-files/files/mods/Linux.bsd.lib.mk
@@ -356,7 +356,7 @@ __archivebuild: .USE
 __archiveinstall: .USE
 	${INSTALL} ${RENAME} ${PRESERVE} ${COPY} ${INSTPRIV} -o ${LIBOWN} \
 	    -g ${LIBGRP} -m 600 ${.ALLSRC} ${.TARGET}
-	${RANLIB} -t ${.TARGET}
+	${RANLIB} ${.TARGET}
 	chmod ${LIBMODE} ${.TARGET}
 
 DPSRCS+=	${SRCS:M*.l:.l=.c} ${SRCS:M*.y:.y=.c}
